650c03b4da61cab405c5ba32a42ffa55e1337aff
Vinícius Vicari <vicari_snoway@hotmail.com>
2022-07-04 13:33:27 -0300

    Adição do uso das rotas e formatação de json com express

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,14 @@ import "reflect-metadata";
 import 'dotenv/config';
 import AppDataSource from "./database/dataSource";
 import express from "express";
+import { routes } from "./routes";
 
 const PORT = process.env.TYPEORM_PORT;
 
 const app = express();
+
+app.use(express.json());
+app.use(routes);
 
 AppDataSource.initialize().then(() => {
     app.listen(PORT, () => {
