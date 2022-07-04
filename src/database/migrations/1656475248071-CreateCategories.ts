e1e04f0ead1b8c136decee5e2eadeb4b9276785b
Vinícius Vicari <vicari_snoway@hotmail.com>
2022-07-04 13:33:54 -0300

    Correção de identação

--- src/database/migrations/1656475248071-CreateCategories.ts
+++ src/database/migrations/1656475248071-CreateCategories.ts
@@ -1,39 +1,36 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm"
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateCategories1656475248071 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "categories",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "description",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.createTable(
-            new Table({
-                name: "categories",
-                columns: [
-                    {
-                        name: "id",
-                        type: "uuid",
-                        isPrimary: true,
-                    },
-                    {
-                        name: "name",
-                        type: "varchar",
-                        isUnique: true,
-                    },
-                    {
-                        name: "description",
-                        type: "varchar",
-                    },
-                    {
-                        name: "created_at",
-                        type: "timestamp",
-                        default: "now()",
-                    }
-                ]
-        })
-        )
-    
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable("categories")
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("categories");
+  }
 }
