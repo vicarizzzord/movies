9fc8f2853c0cbdccc74f4195a63b38a57b2038b7
Vinícius Vicari <vicari_snoway@hotmail.com>
2022-07-04 15:14:40 -0300

    Adição das rotas de Vídeo

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,25 @@
-import {Router} from "express";
+import { Router } from "express";
 import { CreateCategoryController } from "./controllers/_categories/CreateCategoryController";
 import { DeleteCategoryController } from "./controllers/_categories/DeleteCategoryController";
 import { GetAllCategoriesController } from "./controllers/_categories/GetAllCategoriesController";
 import { UpdateCategoryController } from "./controllers/_categories/UpdateCategoryController";
+import { CreateVideoController } from "./controllers/_videos/CreateVideoController";
+import { DeleteVideoController } from "./controllers/_videos/DeleteVideoController";
+import { GetAllVideosController } from "./controllers/_videos/GetAllVideosController";
+import { UpdateVideoController } from "./controllers/_videos/UpdateVideoController";
 
 const routes = Router();
 
+// Categories routes
 routes.post("/categories", new CreateCategoryController().handle);
 routes.get("/categories", new GetAllCategoriesController().handle);
 routes.put("/categories/:id", new UpdateCategoryController().handle);
 routes.delete("/categories/:id", new DeleteCategoryController().handle);
 
+//Videos routes
+routes.post("/videos", new CreateVideoController().handle);
+routes.get("/videos", new GetAllVideosController().handle);
+routes.put("/videos/:id", new UpdateVideoController().handle);
+routes.delete("/videos/:id", new DeleteVideoController().handle);
 
-export {routes}
+export { routes };
